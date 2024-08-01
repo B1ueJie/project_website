d67cfe7aee4af6be49dce0ce59289e9ed0547fbb
Jie Lin <jie.lin828@outlook.com>
2024-07-31 19:33:41 -0600

    Trying out a quick fix to the contentnav key issue

--- components/contentnav.tsx
+++ components/contentnav.tsx
@@ -5,15 +5,13 @@ import Link from 'next/link';
 export default function ContentNav(props: any) {
     const router = useRouter();
     const currentPath = router.asPath;
-    console.log(currentPath);
     const basePath = currentPath.replace(/\/skills&education.*/, '/skills&education/');
-
 
     return (
         <nav>
             <ul className="">
                 {props.props.map(( file: any ) => ( // checking by just the file.id is unsafe, but should be fine with the scale of the website
-                    <li className={`hover:bg-brown hover:text-tan hover:transition-all ${(currentPath.match(file.id)) ? "pointer-events-none bg-brown text-tan" : "bg-yellowgray"}`}>
+                    <li key={file.id} className={`hover:bg-brown hover:text-tan hover:transition-all ${(currentPath.match(file.id)) ? "pointer-events-none bg-brown text-tan" : "bg-yellowgray"}`}>
                         <Link className={`text-xl block p-3 ${(currentPath.match(file.id)) ? "pointer-events-none" : ""}`} href={basePath + file.id}>{file.title}</Link>
                     </li>
                 ))}
