d19c5ee514252305895d7b26270c31ccb2c4bd13
Jie Lin <jie.lin828@outlook.com>
2024-08-08 21:07:42 -0600

    Quick fix for work's dynamic page, forgot i didn't do anything with this yet

--- pages/writing/[work].tsx
+++ pages/writing/[work].tsx
@@ -6,27 +6,27 @@ export default function Work(props: any) {
     );
 }
 
-export async function getStaticProps(context: any) {
-    const data = await getFileData('works', context.params.work);
-    const filesData = await getFilesData('works');
+// export async function getStaticProps(context: any) {
+//     const data = await getFileData('works', context.params.work);
+//     const filesData = await getFilesData('works');
 
-    return {
-        props: {
-            fileContent: data,
-            files: filesData
-        }
-    };
-}
+//     return {
+//         props: {
+//             fileContent: data,
+//             files: filesData
+//         }
+//     };
+// }
 
-export async function getStaticPaths(context: any) {
-    const data = await getFileNames('works');
-    const pathsWithParams = data.map((name: string) => {
-        return {
-            params: {work: name}
-        };
-    });
-    return {
-        paths: pathsWithParams,
-        fallback: false
-    };
-}
+// export async function getStaticPaths(context: any) {
+//     const data = await getFileNames('works');
+//     const pathsWithParams = data.map((name: string) => {
+//         return {
+//             params: {work: name}
+//         };
+//     });
+//     return {
+//         paths: pathsWithParams,
+//         fallback: false
+//     };
+// }
